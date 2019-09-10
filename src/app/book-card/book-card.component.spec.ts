baf57a86375a9de3afef9152e824b8ba01652724
john <johnwee35@gmail.com>
2019-09-10 22:57:56 +0800

    add book card component

---
+++ src/app/book-card/book-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookCardsComponent } from './book-card.component';
+
+describe('BookCardsComponent', () => {
+  let component: BookCardsComponent;
+  let fixture: ComponentFixture<BookCardsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookCardsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookCardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
