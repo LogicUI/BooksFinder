181ea392f0d5a037cf771d2b588ec6b85fba0d18
john <johnwee35@gmail.com>
2019-09-10 22:57:29 +0800

    add book models for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { BooksService } from './services/books.service';
+import { Component } from "@angular/core";
+import { BooksService } from "./services/books.service";
+import { Observable } from "rxjs";
+import { Book } from "./model/book";
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  books$: Observable<Book[]>;
+
   constructor(private bookService: BooksService) {}
 
   onBookSearch(query: string) {
-    this.bookService.getBooks(query);
+    this.books$ = this.bookService.getBooks(query);
   }
 }
